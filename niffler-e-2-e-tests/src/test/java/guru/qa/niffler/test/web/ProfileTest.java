ef5f11d4970bf185663da509bd4d582078849d63
igafarov <igafarov90@.mail>
2026-01-28 00:07:35 +0400

    убрал пусые строки

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
@@ -4,7 +4,9 @@ import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.Category;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.extension.BrowserExtension;
+import guru.qa.niffler.jupiter.extension.CategoryExtension;
 import guru.qa.niffler.jupiter.extension.TestMethodContextExtension;
+import guru.qa.niffler.jupiter.extension.UserExtension;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.ProfilePage;
@@ -12,12 +14,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
 
 
 @ExtendWith(
         {
                 TestMethodContextExtension.class,
+          //      UserExtension.class,
+            //    CategoryExtension.class,
                 BrowserExtension.class
         }
 )
@@ -25,7 +28,6 @@ public class ProfileTest {
     private final static String HEADER = "Profile";
     private final static Config CFG = Config.getInstance();
     ProfilePage profilePage;
-
 
     @Test
     @User(
@@ -52,7 +54,5 @@ public class ProfileTest {
                 .openProfilePage()
                 .checkPageHeader(HEADER)
                 .checkCategoryExists(user.testData().categories().getFirst().name());
-        sleep(5000);
-
     }
 }
